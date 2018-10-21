3500da306b1fae7a3154e853e0e758627158a0ca
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-10-21 10:17:05 -0700

    Data as JS objects

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {creds} from './../config/credentials';
 import './App.css';
+import './../priceData';
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -12,18 +12,11 @@ class App extends Component {
     return (
       <React.Fragment>
         <Header />
-<<<<<<< HEAD
-        <p>
-          {console.log(creds.username)}
-        </p>
-      </div>
-=======
         <article id='page'>
           <SearchForm />
         </article>
         <Footer />
       </React.Fragment>
->>>>>>> b8f0b032216e1094925206e47ce4e9611cfcde04
     );
   }
 }
---
+++ src/priceData.js
@@ -0,0 +1,72 @@
+const data = [
+    {
+        itemName: "gold",
+        price: 39.47,
+        unit: "g",
+        category: "metal",
+        photoURL: "https://cdn.pixabay.com/photo/2014/11/01/22/33/gold-513062_1280.jpg"
+    },
+    {
+        itemName: "silver",
+        price: 0.47,
+        unit: "g",
+        category: "metal",
+        photoURL: "https://cdn.pixabay.com/photo/2017/03/27/21/31/money-2180330_1280.jpg"
+    },
+    {
+        itemName: "platinum",
+        price: 26.88,
+        unit: "g",
+        category: "metal",
+        photoURL: "https://cdn.pixabay.com/photo/2017/03/27/21/31/money-2180330_1280.jpg"
+    },
+    {
+        itemName: "palladium",
+        price: 34.69,
+        unit: "g",
+        category: "metal",
+        photoURL: "https://cdn.pixabay.com/photo/2017/03/27/21/31/money-2180330_1280.jpg"
+    },
+    {
+        itemName: "GloBowl Motion-Activated Toilet Nightlight",
+        price: 9.69,
+        unit: "each",
+        category: "goof",
+        photoURL: "https://images-na.ssl-images-amazon.com/images/I/71tfbRyjMIL._SL1500_.jpg"
+    },
+    {
+        itemName: "Foam Pool Noodles",
+        price: 1.5,
+        unit: "each",
+        category: "goof",
+        photoURL: "https://images-na.ssl-images-amazon.com/images/I/614-xrcjPJL._SX522_.jpg"
+    },
+    {
+        itemName: "Potatoes",
+        price: 0.5,
+        unit: "each",
+        category: "food",
+        photoURL: "https://images-na.ssl-images-amazon.com/images/I/811fGdwqf%2BL._SL1500_.jpg"
+    },
+    {
+        itemName: "Salmon Fish Sushi Pillow Cushion Home Decor",
+        price: 7.09,
+        unit: "each",
+        category: "goof",
+        photoURL: "https://images-na.ssl-images-amazon.com/images/I/416CKdw6LjL.jpg"
+    },
+    {
+        itemName: "Gummi Bear Skeleton Anatomy Model Kit",
+        price: 36.78,
+        unit: "each",
+        category: "goof",
+        photoURL: "https://images-na.ssl-images-amazon.com/images/I/71-7hxETLkL._SL1500_.jpg"
+    },
+    {
+        itemName: "ToePal Gel Separators",
+        price: 17.85,
+        unit: "each",
+        category: "goof",
+        photoURL: "https://images-na.ssl-images-amazon.com/images/I/91n1UCGwJOL._SL1500_.jpg"
+    }
+];
