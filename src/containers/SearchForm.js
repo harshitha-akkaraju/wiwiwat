412e42fe550bb53f11560de18846eb309a43ef29
aamiller <adelem@uw.edu>
2018-10-21 12:38:18 -0700

    Function updates

--- src/containers/SearchForm.js
+++ src/containers/SearchForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './SearchForm.css';
 import {TextField} from '@material-ui/core';
+import { getClosestItem } from '../productFinder';
 
 class SearchForm extends React.Component {
   render() {
+    console.log(getClosestItem(.29, "lb", 2.3));
     return (
       <div className="App">
         <TextField id="weight" label="weight in lbs" fullWidth/>
--- src/productFinder.js
+++ src/productFinder.js
@@ -1,11 +1,10 @@
 // Source in data
-import React from 'react';
 import { data } from './priceData';
 
 // getClosestItem takes in a value of an item and the unit the item is in, and outputs
 // an item and quantity of that item that approximates the value of the input item.
-function getClosestItem(inputItemValue, inputItemUnit, inputItemWeight) {
-    if (inputItemValue == 0 || inputItemWeight == 0) { return ""; }
+export function getClosestItem(inputItemValue, inputItemUnit, inputItemWeight) {
+    if (inputItemValue === 0 || inputItemWeight === 0) { return ""; }
     
     // Convert weight to lbs for us metric avoidant Americans
     let perPoundValue = 0;
@@ -19,35 +18,25 @@ function getClosestItem(inputItemValue, inputItemUnit, inputItemWeight) {
     } else if (inputItemUnit === "kg") {
         perPoundValue = inputItemValue * 2.20462;
         itemWeightInPounds = inputItemValue * 2.20462;
-    } else if (inputItemUnit == "lb") {
+    } else if (inputItemUnit === "lb") {
         perPoundValue = inputItemValue;
     }
     
     let quantities = [];
-    data.forEach( prod => {
-        if (prod.unit == "each") {
-            // If approximately a number of items with quantity "each"
-            if (inputItemValue % prod.price < .2) {
-                quantities.push({
-                    itemName: prod.item, 
-                    quantity: Math.round(inputItemValue / prod.price, 1), 
-                    unit: "each",
-                    distFromWholeNum: 0
-                }); }
-            } else {
-                // Find amount of this item that the input item is equivalent in value to
-                quantities.push({
-                    itemName: prod.item,
-                    quantity: Math.round(inputItemWeight / prod.price, 2),
-                    units: "lb",
-                    distFromWholeNum: inputItemWeight % prod.price
-                })
-            }
-        });  
-        // Sort by quantity, return
-        quantities.sort(function(o1, o2) {
-            return o1.distFromWholeNum - o2.distFromWholeNum;
+    data.forEach(prod => {
+        // Find amount of this item that the input item is equivalent in value to
+        quantities.push({
+            itemName: prod.itemName,
+            quantity: inputItemWeight / prod.price,
+            units: "lb",
+            distFromWholeNum: (inputItemWeight % prod.price) - 1,
+            photoURL: prod.photoURL
         })
-        
-        return quantities;
-    }
+    });  
+    // Sort by quantity, return
+    quantities.sort(function(o1, o2) {
+        return o1.distFromWholeNum - o2.distFromWholeNum;
+    })
+    
+    return quantities;
+}
