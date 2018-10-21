62d5d64fa526e7b97d58379fdff5162d2d342cdc
Ryan Keller <rykeller@uw.edu>
2018-10-21 01:13:51 -0700

    Sticky footer - saved by flexbox!

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        A DubHacks 2018 Project.
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/containers/SearchForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SearchForm.css';
+
+class SearchForm extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    );
+  }
+}
+
+export default SearchForm;
